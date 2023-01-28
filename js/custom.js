c42c2085d8829555b556a0a27e56dc6e5b100bf5
Tiago <tgoncalves94.tg@gmail.com>
2023-01-28 15:36:10 +0100

    [DEV]- New Article

--- js/custom.js
+++ js/custom.js
@@ -42,28 +42,3 @@ document.getElementById('btn mr-lg-2 custom-btn').addEventListener('click', func
   a.download = 'cv.pdf';
   a.click();
 });
-
-const prevBtn = document.querySelector('#prev');
-const nextBtn = document.querySelector('#next');
-
-let currentIndex = 0;
-const articles = [
-  { title: "Article 1", url: "article-1.html" },
-  { title: "Article 2", url: "article-2.html" },
-  { title: "Article 3", url: "article-3.html" },
-  // Add more articles here as needed
-];
-
-prevBtn.addEventListener('click', function() {
-  if (currentIndex > 0) {
-    currentIndex--;
-    location.href = articles[currentIndex].url;
-  }
-});
-
-nextBtn.addEventListener('click', function() {
-  if (currentIndex < articles.length - 1) {
-    currentIndex++;
-    location.href = articles[currentIndex].url;
-  }
-});
--- script/projects.js
+++ script/projects.js
@@ -22,3 +22,81 @@ var fileName = getQueryParameter('file');
 
 // Read the contents of the text file and display it on the page
 readTextFile(fileName);
+
+function getPreviousArticle() {
+    switch(fileName) {
+        // Compétences techniques
+        case "article_android" :
+            window.location.replace("project.html?file=article_maquette");
+            break;
+        case "article_maquette" :
+            window.location.replace("project.html?file=article_algorithmique");
+            break;
+        case "article_algorithmique" :
+            window.location.replace("project.html?file=article_android");
+            break;
+
+        // Compétences transverses
+        case "article_patience" :
+            window.location.replace("project.html?file=article_teamworking");
+            break;
+        case "article_teamworking" :
+            window.location.replace("project.html?file=article_discipline");
+            break;
+        case "article_discipline" :
+            window.location.replace("project.html?file=article_patience");
+            break;
+
+        // Réalisations
+        case "article_ulysse" :
+            window.location.replace("project.html?file=article_eyetrek");
+            break;
+        case "article_eyetrek" :
+            window.location.replace("project.html?file=article_eyetrek");
+            break;
+
+        //Default
+        default : {
+            console.log("Sorry we are ou of ${expr}.");
+        }
+    }
+}
+
+function getNextArticle() {
+    switch(fileName) {
+        // Compétences techniques
+        case "article_android" :
+            window.location.replace("project.html?file=article_algorithmique");
+            break;
+        case "article_maquette" :
+            window.location.replace("project.html?file=article_android");
+            break;
+        case "article_algorithmique" :
+            window.location.replace("project.html?file=article_maquette");
+            break;
+
+        // Compétences transverses
+        case "article_patience" :
+            window.location.replace("project.html?file=article_discipline");
+            break;
+        case "article_teamworking" :
+            window.location.replace("project.html?file=article_patience");
+            break;
+        case "article_discipline" :
+            window.location.replace("project.html?file=article_teamworking");
+            break;
+
+        // Réalisations
+        case "article_ulysse" :
+            window.location.replace("project.html?file=article_eyetrek");
+            break;
+        case "article_eyetrek" :
+            window.location.replace("project.html?file=article_eyetrek");
+            break;
+
+        //Default
+        default : {
+            console.log("Sorry we are ou of ${expr}.");
+        }
+    }
+}
